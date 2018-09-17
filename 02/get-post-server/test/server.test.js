d9d06ae025755ad177f78d505d7e1083355b78fb
Daniyil Vatahovych <danilvat7@gmail.com>
2018-09-18 01:00:19 +0300

    change tests to async await

--- 02/get-post-server/test/server.test.js
+++ 02/get-post-server/test/server.test.js
@@ -1,8 +1,15 @@
 const assert = require('assert');
+const should = require('should');
 const server = require('../server');
 const config = require('config');
 const request = require('request');
 const fs = require('fs-extra');
+
+const rp = require('request-promise').defaults({
+  encoding: null,
+  simple: false,
+  resolveWithFullResponse: true
+});
 
 const host = 'http://127.0.0.1:3000';
 const fixturesRoot = __dirname + '/fixtures';
@@ -24,18 +31,10 @@ describe('app tests', () => {
 
   describe('GET method tests', () => {
     describe('test /', () => {
-      it('should return index.html', done => {
-        request(`${host}`, (err, res, body) => {
-          if (err) return done(err);
-          const file = fs.readFileSync(
-            `${config.get('publicRoot')}/index.html`,
-            {
-              encoding: 'utf-8'
-            }
-          );
-          assert.equal(body, file);
-          done();
-        });
+      it('should return index.html', async () => {
+        const file = fs.readFileSync(`${config.get('publicRoot')}/index.html`);
+        const res = await rp.get(`${host}`);
+        res.body.equals(file).should.be.true();
       });
     });
 
@@ -47,38 +46,24 @@ describe('app tests', () => {
             `${config.get('filesRoot')}/small.png`
           );
         });
-        it('return status 200 and file', done => {
-          request.get(`${host}/small.png`, (err, res, body) => {
-            if (err) return done(err);
-            const file = fs.readFileSync(`${fixturesRoot}/small.png`);
-            assert.equal(file, body);
-            done();
-          });
+        it('return status 200 and file', async () => {
+          const file = fs.readFileSync(`${fixturesRoot}/small.png`);
+          const res = await rp.get(`${host}/small.png`);
+          res.body.equals(file).should.be.true();
         });
       });
       context('otherwise', () => {
-        it('returm Not found', done => {
-          request.get(`${host}/small.png`, (err, res, body) => {
-            if (err) return done(err);
-            assert.equal(res.statusCode, 404);
-            assert.equal(body, 'Not found');
-            done();
-          });
+        it('returm Not found', async () => {
+          const res = await rp.get(`${host}/small.png`);
+          res.statusCode.should.be.equal(404);
         });
       });
     });
 
     describe('nested', () => {
-      it('return 400', done => {
-        request.get(`${host}/..`, (err, res) => {
-          if (err) return done(err);
-          assert.equal(res.statusCode, 400);
-        });
-        request.get(`${host}//`, (err, res) => {
-          if (err) return done(err);
-          assert.equal(res.statusCode, 400);
-        });
-        done();
+      it('return 400', async () => {
+        const res = await rp.get(`${host}/nested/path`);
+        res.statusCode.should.be.equal(400);
       });
     });
   });
@@ -86,64 +71,52 @@ describe('app tests', () => {
   describe('POST method tests', () => {
     describe('test /file', () => {
       context('when save', () => {
-        it('return status 200 and save file', done => {
+        it('return status 200 and save file', async () => {
           const file = fs.readFileSync(`${fixturesRoot}/small.png`);
-          request.post({ url: `${host}/small.png`, body: file }, (err, res) => {
-            if (err) done(err);
-            assert.equal(res.statusCode, 200);
+          const res = await rp.post({
+            url: `${host}/small.png`,
+            body: file
           });
-          done();
+          res.statusCode.should.be.equal(200);
         });
       });
 
       context('when not save', () => {
         describe('too big', () => {
-          it('return 413', done => {
-            const bigFile = fs.readFileSync(`${fixturesRoot}/big.png`);
+          it('return 413', async () => {
+            // const bigFile = fs.readFileSync(`${fixturesRoot}/big.png`);
+            // const res = await rp.post({
+            //   url: `${host}/big.png`,
+            //   body: bigFile
+            // });
+            // res.statusCode.should.be.equal(413);
 
-            request.post(
-              { url: `${host}/big.png`, body: bigFile },
-              (err, res) => {
-                if (err) done(err);
-                assert.equal(res.statusCode, 413);
-              }
-            );
-
-            done();
           });
         });
 
         describe('exist', () => {
-          it('return 409', done => {
+          it('return 409', async () => {
             fs.copySync(
               `${fixturesRoot}/small.png`,
               `${config.get('filesRoot')}/small.png`
             );
             const file = fs.readFileSync(`${fixturesRoot}/small.png`);
-
-            request.post(
-              { url: `${host}/small.png`, body: file },
-              (err, res) => {
-                if (err) done(err);
-                assert.equal(res.statusCode, 409);
-              }
-            );
-            done();
+            const res = await rp.post({
+              url: `${host}/small.png`,
+              body: file
+            });
+            res.statusCode.should.be.equal(409);
           });
         });
 
         describe('not found', () => {
-          it('return 404', done => {
+          it('return 404', async () => {
             const file = fs.readFileSync(`${fixturesRoot}/small.png`);
-
-            request.post(
-              { url: `${host}`, body: file },
-              (err, res) => {
-                if (err) done(err);
-                assert.equal(res.statusCode, 404);
-              }
-            );
-            done();
+            const res = await rp.post({
+              url: `${host}`,
+              body: file
+            });
+            res.statusCode.should.be.equal(404);
           });
         });
       });
